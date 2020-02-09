b380ec3440390ec21de55003b5f47bae8c163146
Aziz Nurfalah <aziznurfalah@gmail.com>
2020-02-09 16:57:30 +0700

    print log

--- application/views/root/_footer.php
+++ application/views/root/_footer.php
@@ -69,6 +69,7 @@
   });
 
   function signOut() {
+    console.log(gapi.auth2);
     var auth2 = gapi.auth2.getAuthInstance();
     auth2.signOut().then(function () {
       console.log('User signed out.');
